15f92a0f71c5d1425306be5ae231ffe4e7ad58f1
Christian <christian.husunu@gmail.com>
2020-10-29 07:23:37 +0000

    Rewrite parser via `scan`

--- Lisp2Swift/main.swift
+++ Lisp2Swift/main.swift
@@ -2,43 +2,104 @@ import Foundation
 
 print("Hello, World!")
 
-struct Expression {
-    let symbol: String
-    let parameters: [String]
+extension String {
+    
+    func enclosed(by: String) -> Bool {
+        return hasPrefix(by) && hasSuffix(by)
+    }
+    
+    var isExpression: Bool {
+        return hasPrefix("(") && hasSuffix(")")
+    }
 }
 
-enum ScanResult: Equatable {
-    case regions(_: [String])
-    case incomplete
+enum Word: Equatable {
+    case string(_: String)
+    case symbol(_: String)
+    case expression(_: [Word])
+    case invalid(_: String)
+    
+    init?(string: String) {
+        if string.enclosed(by: "\"") {
+            self = .string(string)
+        }
+        else if string.contains("(") || string.contains(")") {
+            self = .invalid(string)
+        }
+        else if string.count == 0 {
+            return nil
+        }
+        else {
+            self = .symbol(string)
+        }
+    }
+}
+
+enum Expression: Equatable {
+    case string(_: String)
+    case symbol(_: String)
+    case expression(_: [Expression])
+}
+
+enum ParseResult: Equatable {
+    case valid(expressions: [Expression])
+    case invalid(expression: String)
 }
 
 class Parser {
+      
+    func scan(text: String) -> [Word] {
+        if text.isExpression {
+            let contents = (text as NSString).substring(with: NSMakeRange(1, text.count - 2))
+            return [.expression(scan(text: contents))]
+        }
+        else if text.contains(" ") {
+            let parts = text.split(separator: " ")
+                .compactMap({String($0)})
+                .filter({$0.count > 0})
+            return parts.compactMap({scan(text: $0).first})
+        }
+        else {
+            return [Word(string: text)].compactMap({$0})
+        }
+    }
 
-    func scan(text: String) -> ScanResult {
-        return .regions([""])
+    func parse(text: String) -> ParseResult {
+        let words = scan(text: text)
+        // TODO: check that first tier only has expressions
+        return .valid(expressions: [])
     }
     
-    func parse(lispString: String) -> [Expression] {
-        
-        let string = lispString as NSString
-        let startRange = string.range(of: "(")
-        let endRange = string.range(of: ")")
-        
-        guard startRange.location != NSNotFound && endRange.location != NSNotFound else {
-            return []
-        }
-        
-        let range = NSMakeRange(startRange.location + 1, endRange.location-startRange.location - 1)
-        let contents = string.substring(with: range)
-        
-        let elements = contents.split(separator: " ").map({String($0)})
-        
-        guard elements.count >= 2 else {
-            return []
-        }
-        
-        let symbol = elements[0]
-        let parameters = Array(elements.dropFirst())
-        return [Expression(symbol: symbol, parameters: parameters)]
-    }
+    
+    
+//    // OLD IMPL
+//
+//    struct Expression2 {
+//        let symbol: String
+//        let parameters: [String]
+//    }
+//
+//    func oldParse(lispString: String) -> [Expression2] {
+//
+//        let string = lispString as NSString
+//        let startRange = string.range(of: "(")
+//        let endRange = string.range(of: ")")
+//
+//        guard startRange.location != NSNotFound && endRange.location != NSNotFound else {
+//            return []
+//        }
+//
+//        let range = NSMakeRange(startRange.location + 1, endRange.location-startRange.location - 1)
+//        let contents = string.substring(with: range)
+//
+//        let elements = contents.split(separator: " ").map({String($0)})
+//
+//        guard elements.count >= 2 else {
+//            return []
+//        }
+//
+//        let symbol = elements[0]
+//        let parameters = Array(elements.dropFirst())
+//        return [Expression2(symbol: symbol, parameters: parameters)]
+//    }
 }
--- Lisp2SwiftTests/Lisp2SwiftTests.swift
+++ Lisp2SwiftTests/Lisp2SwiftTests.swift
@@ -5,38 +5,53 @@ class Lisp2SwiftTests: XCTestCase {
 
     let parser = Parser()
     
-    func scan(_ text: String) -> ScanResult {
+    func scan(_ text: String) -> [Word] {
         return parser.scan(text: text)
     }
     
     func test_scan_emptyText() {
-        XCTAssertEqual(scan(""), .regions([]))
+        XCTAssertEqual(scan(""), [])
     }
     
-    func test_scan_invalidText() {
-        XCTAssertEqual(scan("bla"), .incomplete)
+    func test_scan_doubleQuotedString() {
+        XCTAssertEqual(scan("\"hello\""), [.string("\"hello\"")])
     }
     
-    func test_scan_unended_expression() {
-        XCTAssertEqual(scan("(bla"), .incomplete)
+    func test_scan_stringAndSymbol() {
+        XCTAssertEqual(scan("\"hello\" foo"), [.string("\"hello\""), .symbol("foo")])
     }
     
-    func test_scan_expression() {
-        XCTAssertEqual(scan("(bla)"), .regions(["bla"]))
+    func test_scan_expressionWithDoubleQuotedString() {
+        XCTAssertEqual(scan("(\"hello\")"), [.expression([.string("\"hello\"")])])
     }
     
-    func test_scan_twoExpressions() {
-        XCTAssertEqual(scan("(bla)(bla)"), .regions(["bla", "bla"]))
+    func test_scan_emptyExpression() {
+        XCTAssertEqual(scan("()"), [.expression([])])
     }
+    
+    func test_scan_multipleParameterExpression() {
+        XCTAssertEqual(scan("(hello world)"), [.expression([.symbol("hello"), .symbol("world")])])
+    }
+    
+    func test_scan_unEndedExpression() {
+        XCTAssertEqual(scan("("), [.invalid("(")])
+    }
+    
+    func test_scan_unStartedExpression() {
+        XCTAssertEqual(scan(")"), [.invalid(")")])
+    }
+    
+    func test_scan_invalidExpression() {
+        XCTAssertEqual(scan("basdf(foo"), [.invalid("basdf(foo")])
+    }
+    
+    ///
     
     func test_parse_print_expression() throws {
         let lisp = """
         (print "hi")
         """
-        let expressions = parser.parse(lispString: lisp)
-        XCTAssertEqual(expressions.count, 1)
-        let first = try XCTUnwrap(expressions.first)
-        XCTAssertEqual(first.symbol, "print")
-        XCTAssertEqual(first.parameters, ["\"hi\""])
+        let result = parser.parse(text: lisp)
+        XCTAssertEqual(result, .valid(expressions: [.symbol("print"), .string("\"hi\"")]))        
     }
 }
